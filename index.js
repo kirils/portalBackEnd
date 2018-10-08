4529cfa692c00be2dea0bde456dcb8a94500b4f1
Will Hill <email.will.in.china@gmail.com>
2018-10-08 17:37:50 +0800

    version one

---
+++ index.js
@@ -0,0 +1,42 @@
+'use strict';
+const csv = require('fast-csv');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/snapshot', { useNewUrlParser: true });
+
+const snapshotSchema = new mongoose.Schema({
+    account_name:   { type: String, required: true, unique: true},
+    owner_key:      { type: String, required: true, unique: true},
+    active_key:     { type: String, required: true, unique: true},
+    total_nostake:  { type: Number, required: true},
+    staked:         { type: Number, required: true},
+    delegated:      { type: Number, required: true},
+    total:          { type: Number, required: true},
+    created_at:     Date,
+    updated_at:     Date
+  });
+
+  const Account = mongoose.model('Account', snapshotSchema);
+
+  csv.fromPath("snapshot-final.csv")
+  .on("data", (data) => {
+    
+    const account_name = data[1];
+    const owner_key = data[2];
+    const active_key = data[3];
+    const total_nostake = parseFloat(data[4]) || 0;
+    const staked = parseFloat(data[5]) || 0;
+    const delegated = parseFloat(data[6]) || 0;
+    const total = parseFloat(data[7]) || 0;
+
+    const recordToInsert = new Account({account_name, owner_key, active_key, total_nostake, staked, delegated, total });
+
+    recordToInsert.save((err) => {
+        if (err) console.log(err);
+    });
+
+    console.log(new Date().getTime());
+
+  })
+  .on("end", () => console.log(`Done`));
+
