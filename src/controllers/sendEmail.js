5538e21500acd5ed965079ac0319ea08133404e1
Will Hill <email.will.in.china@gmail.com>
2018-10-12 17:36:39 +0800

    test email

--- src/controllers/sendEmail.js
+++ src/controllers/sendEmail.js
@@ -1,7 +1,8 @@
 const aws_access_key_id = process.env.AWS_ACCESS_KEY_ID
 const aws_secret_access_key = process.env.AWS_SECRET_ACCESS_KEY
 var AWS = require('aws-sdk');
-var uuid = require('uuid');
+AWS.config.update({region: 'us-east-1'});
+
 
 
 
@@ -11,7 +12,28 @@ var uuid = require('uuid');
  * @returns {email recipt}
  */
 function create(req, res) {
-
+    var params = {
+        Destination: {ToAddresses: ['william@worbli.io']},
+        Message: {
+            Body: { 
+                Html: {Charset: "UTF-8", Data: "<h1>test</h1>"},
+                Text: {Charset: "UTF-8", Data: "test"}
+                },
+            Subject: {Charset: 'UTF-8', Data: '[WORBLI] Email Validation'}
+        },
+        Source: 'do-not-reply@worbli.io', 
+        ReplyToAddresses: ['do-not-reply@worbli.io'],
+      };       
+    
+    var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+    
+    sendPromise
+    .then((data) => {
+        console.log(data.MessageId);
+    })
+    .catch((err) => {
+        console.error(err, err.stack);
+    });
 
 }
 
