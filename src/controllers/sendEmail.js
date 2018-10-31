9036a8cad8f7c2ead36872801f5e855bbf663f00
Will Hill <email.will.in.china@gmail.com>
2018-10-31 12:34:59 +0800

    send email register

--- src/controllers/sendEmail.js
+++ src/controllers/sendEmail.js
@@ -1,72 +1,75 @@
 var AWS = require('aws-sdk');
 AWS.config.update({ "accessKeyId": process.env.AWS_ACCESS_KEY_ID, "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY, "region": "us-east-1" });
+const regusterUserModel = require('../models/registerUser.js')
 /**
  * Send an email verification email 
  * @returns {email recipt}
  */
 function validate(req, res) {
+  
   const data = req.params.email.split("~");
-    // TODO: the tillde isnt right we should pass params
   const email = data[0];
-  const securityCode = data[1];
-  // TODO: Save security code and email to database
+  const security_code = data[1];
 
-    var htmlEmail = `
-    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-    <html xmlns="http://www.w3.org/1999/xhtml">
-      <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>[WORBLI] Email Confirmation</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      </head>
-      <body style="margin: 0; padding: 0; background-color: #F7F7F7; min-height: 100vh">
-      <table style="margin: 0; padding: 60px; background-color: #F7F7F7;" cellpadding="0" cellspacing="0" width="100%">
+  var htmlEmail = `
+  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+  <html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <title>[WORBLI] Email Confirmation</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body style="margin: 0; padding: 0; background-color: #F7F7F7; min-height: 100vh">
+    <table style="margin: 0; padding: 60px; background-color: #F7F7F7;" cellpadding="0" cellspacing="0" width="100%">
+      <tr>
+      <td style="text-align:center">
+        <img src="https://www.dac.city/esm-bundled/images/email-logo.png" width="200px" style="margin-bottom: 20px;">
+        <table style="margin: 0; padding: 60px; background-color: #FFFFFF; text-align:left; border-radius: 0.4em;" cellpadding="0" cellspacing="0" width="100%">
         <tr>
-        <td style="text-align:center">
-          <img src="https://www.dac.city/esm-bundled/images/email-logo.png" width="200px" style="margin-bottom: 20px;">
-          <table style="margin: 0; padding: 60px; background-color: #FFFFFF; text-align:left; border-radius: 0.4em;" cellpadding="0" cellspacing="0" width="100%">
-          <tr>
-            <td><b>Confirm Your Registration</b></td>
-          </tr>  
-          <tr>
-            <td>
-            </br></br></br>
-              <p>Welcome to WORBLI!</p>
-              <p>Click the button below to confirm your email.</p>
-              <a href="https://www.dac.city/dashboard/profile/${securityCode}/"><button type="button" style="cursor: pointer; outline: none; font-weight: 600; border-radius:3px; background-color: #37527A; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 15px; width:250px">Verify Email</button></a></br></br>
-              <p>If you can't confirm by clicking the button above, please copy the address below to the browser address bar to confirm.</p>
-              <a href="https://www.dac.city/dashboard/profile/${securityCode}/">https://www.dac.city/dashboard/profile/${securityCode}/</a></br></br></br>
-              <p>If this activity is not your own operation, please contact us immediatly via support@worbli.io</p>
-            </td>
-            </tr>
-          </table>
-        </td>
-        </tr>
-      </table>
-      </body>
-    </html>
-    `
-    var params = {
+          <td><b>Confirm Your Registration</b></td>
+        </tr>  
+        <tr>
+          <td>
+          </br></br></br>
+            <p>Welcome to WORBLI!</p>
+            <p>Click the button below to confirm your email.</p>
+            <a href="https://www.dac.city/dashboard/profile/${security_code}/"><button type="button" style="cursor: pointer; outline: none; font-weight: 600; border-radius:3px; background-color: #37527A; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 15px; width:250px">Verify Email</button></a></br></br>
+            <p>If you can't confirm by clicking the button above, please copy the address below to the browser address bar to confirm.</p>
+            <a href="https://www.dac.city/dashboard/profile/${security_code}/">https://www.dac.city/dashboard/profile/${security_code}/</a></br></br></br>
+            <p>If this activity is not your own operation, please contact us immediatly via support@worbli.io</p>
+          </td>
+          </tr>
+        </table>
+      </td>
+      </tr>
+    </table>
+    </body>
+  </html>
+  `
+
+
+  regusterUserModel({email, security_code}).save((err) => {
+    if (err) {
+        res.json({err})
+    } else {
+      var params = {
         Destination: {ToAddresses: [email]},
         Message: {
-            Body: { 
-                Html: {Charset: "UTF-8", Data: htmlEmail}
-                },
-            Subject: {Charset: 'UTF-8', Data: '[WORBLI] Email Validation'}
+          Body: { 
+            Html: {Charset: "UTF-8", Data: htmlEmail}
+          },
+          Subject: {Charset: 'UTF-8', Data: '[WORBLI] Email Validation'}
         },
-        Source: 'do-not-reply@worbli.io', 
-        ReplyToAddresses: ['do-not-reply@worbli.io'],
-      };       
-    
-   var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-    
+      Source: 'do-not-reply@worbli.io', 
+      ReplyToAddresses: ['do-not-reply@worbli.io'],
+    };       
+      
+    var sendPromise = new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();  
     sendPromise
-    .then((data) => {
-        return res.send(true);
-    })
-    .catch((err) => {
-        return res.send(false);
-    });
+    .then((data) => {return res.send(true)})
+    .catch((err) => {return res.send(false);});
+    };
+  });
 }
 
 /**
--- src/models/registerUser.js
+++ src/models/registerUser.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const registerUserSchema = new mongoose.Schema({
-    first_name:             { type: String, required: true},
-    last_name:              { type: String, required: true},
-    country_residence:      { type: String, required: true},
-    password:               { type: String, required: true},
-    mainnet_account_name:   { type: String, required: true},
-    worbli_account_name:    { type: String, required: true},
+    email:                  { type: String, required: true, unique: true },
+    security_code:          { type: String, required: true, unique: true },
+    first_name:             { type: String },
+    last_name:              { type: String },
+    country_residence:      { type: Number },
+    password:               { type: String },
+    mainnet_account_name:   { type: String },
+    worbli_account_name:    { type: String },
     worbli_account_created: { type: Date },
-    authenteq_id:           { type: Number },
-    security_code:          { type: String, required: true, unique: true},
     created_at:             Date,
     updated_at:             Date
 });
