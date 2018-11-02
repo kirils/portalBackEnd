3f412106428f4ad3bc286d80c7087772c7ea96e5
Will Hill <email.will.in.china@gmail.com>
2018-11-02 09:16:33 +0800

    rate limit

--- src/express.js
+++ src/express.js
@@ -2,10 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const compress = require('compression');
 const cors = require('cors');
+const rateLimit = require("express-rate-limit");
 const helmet = require('helmet');
 const routes = require('./router.js');
-const app = express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+
+const app = express();
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compress());
