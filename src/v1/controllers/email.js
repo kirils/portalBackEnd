2c1d0b002ed38ac2e3bbae9fc4415ac372527be3
Will Hill <email.will.in.china@gmail.com>
2018-11-07 00:04:28 +0800

    test import

--- src/v1/controllers/email.js
+++ src/v1/controllers/email.js
@@ -1,8 +1,8 @@
 //const registerUserModel = require('../models/registerUser.js')
-
+const jwt = require('../components/jwt.js');
 
 function post_authorize(req, res) {
-    console.log(req.body)
+    console.log(jwt.jwt_decode('eyJhbGciOiJIUzI1NiJ9.aGVsbG8.WPGNqbPCBkTVCltUUwXd-aJ7-azUbDV7ABHLdz-9c6o'))
     res.json(true)
 }
 function post_welcome(req, res) {
