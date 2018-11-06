c6ddf4d32953d4f8380215b2f01f971499059dd8
Will Hill <email.will.in.china@gmail.com>
2018-11-07 00:41:22 +0800

    cors options

--- src/express.js
+++ src/express.js
@@ -11,13 +11,20 @@ const limiter = rateLimit({
     max: 100
 });
 
+const corsOptions = {
+    origin: 'http://localhost:9000',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    optionsSuccessStatus: 200
+}
+
 const app = express();
 app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compress());
 app.use(helmet());
-app.use(cors({origin: 'http://localhost:9000'}));
+app.use(cors(corsOptions));
 app.use('/', routes);
 
 module.exports = app;
