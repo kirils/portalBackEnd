70cf972d5615175c06c21f4b491c5f8340b0918b
Will Hill <email.will.in.china@gmail.com>
2018-11-11 11:58:51 +0800

    env link

--- src/v1/components/email.js
+++ src/v1/components/email.js
@@ -4,6 +4,7 @@ AWS.config.update({ "accessKeyId": process.env.AWS_ACCESS_KEY_ID, "secretAccessK
 function send_email(email, newjwt, template) {
     return new Promise(function(resolve, reject) {
         if(template === 'authorize'){
+            var url = process.env.FRONT_END_URL
             var htmlEmail = `
             <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
             <html xmlns="http://www.w3.org/1999/xhtml">
@@ -16,7 +17,7 @@ function send_email(email, newjwt, template) {
               <table style="margin: 0; padding: 60px; background-color: #F7F7F7;" cellpadding="0" cellspacing="0" width="100%">
                 <tr>
                 <td style="text-align:center">
-                  <img src="https://portal-b.worbli.io/esm-bundled/images/email-logo.png" width="200px" style="margin-bottom: 20px;">
+                  <img src="${url}/esm-bundled/images/email-logo.png" width="200px" style="margin-bottom: 20px;">
                   <table style="margin: 0; padding: 60px; background-color: #FFFFFF; text-align:left; border-radius: 0.4em;" cellpadding="0" cellspacing="0" width="100%">
                   <tr>
                     <td><b>Confirm Your Registration</b></td>
@@ -26,9 +27,9 @@ function send_email(email, newjwt, template) {
                     </br></br></br>
                       <p>Welcome to WORBLI!</p>
                       <p>Click the button below to confirm your email.</p>
-                      <a href="https://portal-b.worbli.io/dashboard/profile/${newjwt}/"><button type="button" style="cursor: pointer; outline: none; font-weight: 600; border-radius:3px; background-color: #37527A; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 15px; width:250px">Verify Email</button></a></br></br>
+                      <a href="${url}/dashboard/profile?token=${newjwt}/"><button type="button" style="cursor: pointer; outline: none; font-weight: 600; border-radius:3px; background-color: #37527A; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 15px; width:250px">Verify Email</button></a></br></br>
                       <p>If you can't confirm by clicking the button above, please copy the address below to the browser address bar to confirm.</p>
-                      <a href="https://portal-b.worbli.io/dashboard/profile/${newjwt}/">https://portal-b.worbli.io/dashboard/profile/${newjwt}/</a></br></br></br>
+                      <a href="${url}/dashboard/profile?token=${newjwt}/">${url}/dashboard/profile?token=${newjwt}/</a></br></br></br>
                       <p>If this activity is not your own operation, please contact us immediatly via support@worbli.io</p>
                     </td>
                     </tr>
