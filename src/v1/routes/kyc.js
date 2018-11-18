72984403135330d1479077e4fa1a8a59b9542a7a
Will Hill <email.will.in.china@gmail.com>
2018-11-18 13:50:13 +0800

    post

--- src/v1/routes/kyc.js
+++ src/v1/routes/kyc.js
@@ -8,6 +8,6 @@ const post_applicant = require('./validators/post_applicant.js');
 router.route('/applicant/').post(validate(post_applicant.validate), kycController.post_applicant);
 router.route('/applicant/').get(kycController.get_applicant);
 router.route('/check/').get(kycController.get_check);
-router.route('/webhook/').get(kycController.post_webhook);
+router.route('/webhook/').post(kycController.post_webhook);
 
 module.exports = router;
