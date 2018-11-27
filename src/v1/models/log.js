511f74b74aae213710e5f9d0bc7e62ec133bea3b
Will Hill <email.will.in.china@gmail.com>
2018-11-27 14:46:41 +0800

    log date

--- src/v1/models/log.js
+++ src/v1/models/log.js
@@ -4,7 +4,7 @@ const logSchema = new mongoose.Schema({
   email:          { type: String, required: true },
   data:           { type: String },
   browser:        { type: String },
-  created_at:     { type: Date, required: true },
+  created_at:     { type: Date, default: Date.now },
   action:         { type: String, required: true },
   from:           { type: String, required: true },
 });
